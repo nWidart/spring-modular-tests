0acb6047e8bb97b0b1790ed810669ef091fef485
Nicolas Widart <n.widart@gmail.com>
2018-04-12 21:30:17 +0200

    Rename endpoint

--- application/src/main/java/com/nwidart/controllers/CustomerController.java
+++ application/src/main/java/com/nwidart/controllers/CustomerController.java
@@ -19,7 +19,7 @@ public class CustomerController {
   }
 
   @GetMapping
-  public Iterable<Customer> greeting() {
+  public Iterable<Customer> customers() {
     return customerService.findAll();
   }
 }
