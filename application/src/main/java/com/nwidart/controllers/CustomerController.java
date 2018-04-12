13a2584f5ee728594e196ac3f316005bd662c61e
Nicolas Widart <n.widart@gmail.com>
2018-04-12 21:27:53 +0200

    Removing property

--- application/src/main/java/com/nwidart/controllers/CustomerController.java
+++ application/src/main/java/com/nwidart/controllers/CustomerController.java
@@ -1,20 +1,16 @@
 package com.nwidart.controllers;
 
-import com.nwidart.dto.Greeting;
 import com.nwidart.model.Customer;
 import com.nwidart.service.CustomerService;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
 
-  private final AtomicLong counter = new AtomicLong();
   private CustomerService customerService;
 
   @Autowired
